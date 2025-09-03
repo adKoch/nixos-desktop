42bcfed5da3b0b810b665087a20df7b4b82c06ce
adam koc <adam.kochanski.97@gmail.com>
2025-09-03 19:43:23 +0200

    Add github cli

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, firefox-addons }@inputs: {
+    inherit (self) outputs;
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.adam = import ./home.nix;
+	        home-manager.extraSpecialArgs = { inherit inputs; };
+        }
+      ];
+      specialArgs = { inherit inputs; };
+    };
+  };
+}
---
+++ programs/media.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, inputs, lib, ... }:
+
+{
+  #nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+   # "steam"
+   # "steam-original"
+   # "steam-unwrapped"
+  #  "steam-run"
+  #];
+  #programs.steam = {
+  #  enable = true;
+    #remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  #  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  #  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  #};
+  programs.firefox = {
+    enable = true;
+    #profiles.default = {
+    #  extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+    #    bitwarden
+    #    ublock-origin
+     #   betterttv
+     #   darkreader
+     # ];
+
+     # settings = {
+     #   "browser.startup.homepage" = "https://nixos.org";
+     #   "privacy.resistFingerprinting" = true;
+      #};
+    #};
+  };
+}
