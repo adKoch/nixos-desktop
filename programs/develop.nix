e0f0745717383377dd35db48ed32d48a5450c97b
adam koc <adam.kochanski.97@gmail.com>
2025-09-13 16:06:57 +0200

    Add starship and git branch view in terminal

---
+++ programs/develop.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, inputs, lib, ... }:
+
+{
+  programs.starship = {
+  enable = true;
+  settings = {
+    git_branch = {
+      format = "[$symbol$branch]($style) ";
+      style = "bright-green";
+    };
+  };
+};
+environment.interactiveShellInit = ''
+    eval "$(${pkgs.starship}/bin/starship init bash)"
+  '';
+
+  # Or for a custom Git branch prompt without Starship:
+  programs.bash.promptInit = ''
+    parse_git_branch() {
+      ${pkgs.git}/bin/git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+    }
+
+    PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
+  '';
+}
