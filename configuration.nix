c954356619025866e810be334bc05e8e7cbd3554
adam koc <adam.kochanski.97@gmail.com>
2025-11-15 18:06:45 +0100

    Add wallpaper

--- configuration.nix
+++ configuration.nix
@@ -61,6 +61,31 @@
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+  
+  # Set wallpaper
+  services.displayManager.sddm.settings = {
+    Theme = {
+      Current = "breeze";
+    };
+  };
+  
+  # Configure wallpaper for KDE Plasma
+  environment.etc."xdg/plasma-workspace/env/set_wallpaper.sh" = {
+    text = ''
+      #!/bin/bash
+      sleep 5
+      qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "
+        var allDesktops = desktops();
+        for (i=0;i<allDesktops.length;i++) {
+          d = allDesktops[i];
+          d.wallpaperPlugin = 'org.kde.image';
+          d.currentConfigGroup = Array('Wallpaper', 'org.kde.image', 'General');
+          d.writeConfig('Image', 'file:///etc/nixos/bg.png');
+        }
+      "
+    '';
+    mode = "0755";
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
