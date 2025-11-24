65c3cd8d8f2d786eaa382b8b36cc1591e6801fda
adam koc <adam.kochanski.97@gmail.com>
2025-11-24 19:45:37 +0100

    Fix hostname

--- rebuild.sh
+++ rebuild.sh
@@ -4,6 +4,6 @@ nix flake update
 
 sudo nix-channel --update
 
-sudo NIXPKGS_ALLOW_UNFREE=1 nixos-rebuild switch --flake . --impure
+sudo bash -c 'ulimit -n 1048576; NIXPKGS_ALLOW_UNFREE=1 nixos-rebuild switch --flake . --impure --max-jobs 3 --cores 3 --option sandbox false'
 
-nix-collect-garbage --delete-older-than 2d
+#nix-collect-garbage --delete-older-than 2d
